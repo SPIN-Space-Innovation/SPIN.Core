0756bf812944d2e00a00c87ab6e1fd8cda2c2fca
Rikarnto Bariampa <r.bariampa@space-innovation.org>
2023-06-11 17:54:29 +0300

    Made CanInstall in IInstaller a function.

--- dotnet/Installers/Abstractions/IInstaller.cs
+++ dotnet/Installers/Abstractions/IInstaller.cs
@@ -28,5 +28,7 @@ public interface IInstaller
 {
     public ulong Priority { get; }
 
+    public bool CanInstall(IServiceCollection serviceCollection, IConfiguration configuration);
+
     public void InstallService(IServiceCollection serviceCollection, IConfiguration configuration);
 }
